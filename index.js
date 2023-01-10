bc3a4b14ca19f90d5337efcb6ffb8438bed2d46c
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-01-10 20:41:15 +0600

    done

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-
-const run = async () => {
+ const run = async () => {
   try {
     const db = client.db("jobbox");
     const userCollection = db.collection("user");
@@ -38,8 +37,92 @@ const run = async () => {
       if (result?.email) {
         return res.send({ status: true, data: result });
       }
-    
+
       res.send({ status: false });
+    });
+
+    app.patch("/apply", async (req, res) => {
+      const userId = req.body.userId;
+      const jobId = req.body.jobId;
+      const email = req.body.email;
+
+      const filter = { _id: ObjectId(jobId) };
+      const updateDoc = {
+        $push: { applicants: { id: ObjectId(userId), email } },
+      };
+
+      const result = await jobCollection.updateOne(filter, updateDoc);
+
+      if (result.acknowledged) {
+        return res.send({ status: true, data: result });
+      }
+
+      res.send({ status: false });
+    });
+
+    app.patch("/query", async (req, res) => {
+      const userId = req.body.userId;
+      const jobId = req.body.jobId;
+      const email = req.body.email;
+      const question = req.body.question;
+
+      const filter = { _id: ObjectId(jobId) };
+      const updateDoc = {
+        $push: {
+          queries: {
+            id: ObjectId(userId),
+            email,
+            question: question,
+            reply: [],
+          },
+        },
+      };
+
+      const result = await jobCollection.updateOne(filter, updateDoc);
+
+      if (result?.acknowledged) {
+        return res.send({ status: true, data: result });
+      }
+
+      res.send({ status: false });
+    });
+
+    app.patch("/reply", async (req, res) => {
+      const userId = req.body.userId;
+      const reply = req.body.reply;
+      console.log(reply);
+      console.log(userId);
+
+      const filter = { "queries.id": ObjectId(userId) };
+
+      const updateDoc = {
+        $push: {
+          "queries.$[user].reply": reply,
+        },
+      };
+      const arrayFilter = {
+        arrayFilters: [{ "user.id": ObjectId(userId) }],
+      };
+
+      const result = await jobCollection.updateOne(
+        filter,
+        updateDoc,
+        arrayFilter
+      );
+      if (result.acknowledged) {
+        return res.send({ status: true, data: result });
+      }
+
+      res.send({ status: false });
+    });
+
+    app.get("/applied-jobs/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { applicants: { $elemMatch: { email: email } } };
+      const cursor = jobCollection.find(query).project({ applicants: 0 });
+      const result = await cursor.toArray();
+
+      res.send({ status: true, data: result });
     });
 
     app.get("/jobs", async (req, res) => {
@@ -73,5 +156,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port mozahid ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
